d9929d7e6ddef182f31208e80540d91d4bf3faed
Arvind Rangan <3113987+arangan@users.noreply.github.com>
2021-07-12 16:23:38 -0600

    fixed issues with the component

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,3 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-</template>
-
-<script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
-
-export default defineComponent({
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-});
-</script>
-
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -22,6 +5,60 @@ export default defineComponent({
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 10px;
+  margin-left: 10px;
+  margin-right: 10px;
 }
 </style>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import InputFlex from './components/InputFlex.vue';
+import { Doc } from './models/';
+import './assets/app.scss';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    InputFlex
+  },
+  data() {
+    return {
+      doc: new Doc()
+    };
+  },
+  methods: {
+    OnClick_Btn(): void {
+      this.doc.Header;
+    }
+  }
+});
+</script>
+
+<template>
+  <div class="grid">
+    <div class="logo"><input-flex v-model="doc.Header.symbol" /></div>
+
+    <div class="rightAlign">राग</div>
+    <div class="colon">:</div>
+    <div class="leftAlign"><input-flex v-model="doc.Header.raagam" /></div>
+    <div class="rightAlign">आराेह</div>
+    <div class="colon">:</div>
+    <div class="leftAlign">
+      <input-flex v-model="doc.Header.arohanam" />
+    </div>
+
+    <div class="rightAlign">ताल</div>
+    <div class="colon">:</div>
+    <div class="leftAlign">
+      <input-flex v-model="doc.Header.taalam" />
+    </div>
+    <div class="rightAlign">अवरोह</div>
+    <div class="colon">:</div>
+    <div class="leftAlign">
+      <input-flex v-model="doc.Header.avarohanam" />
+    </div>
+  </div>
+
+  <br />
+</template>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from 'vue';
+import App from './App.vue';
+//import InputFlex from "./components/InputFlex.vue";
 
-createApp(App).mount("#app");
+//const app = createApp({});
+//app.component("InputFlex", { InputFlex });
+
+createApp(App).mount('#app');
